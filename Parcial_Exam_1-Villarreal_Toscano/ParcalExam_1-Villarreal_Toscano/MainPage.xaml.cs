787f527d0be8f74d6561f9e876f13d6f90ec1d58
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2022-03-05 23:56:52 -0600

    Finally

---
+++ Parcial_Exam_1-Villarreal_Toscano/ParcalExam_1-Villarreal_Toscano/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ParcalExam_1_Villarreal_Toscano
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
